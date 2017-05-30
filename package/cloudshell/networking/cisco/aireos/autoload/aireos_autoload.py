8ac3ac659b42d46545f0d8f22919a1b806dc7b3a
Yaroslav Nikonorov <yar@MacBook-Pro-Yaroslav.local>
2017-05-30 17:47:31 +0300

    Changed autoload

--- package/cloudshell/networking/cisco/aireos/autoload/aireos_autoload.py
+++ package/cloudshell/networking/cisco/aireos/autoload/aireos_autoload.py
@@ -1,50 +1,31 @@
-from cloudshell.networking.autoload.networking_model import RootElement, Chassis, Port, PortChannel
-from cloudshell.networking.autoload.networking_attributes import RootAttributes, ChassisAttributes, PortAttributes, \
-    PortChannelAttributes
-from cloudshell.networking.operations.interfaces.autoload_operations_interface import AutoloadOperationsInterface
-from cloudshell.shell.core.config_utils import override_attributes_from_config
-import inject
-from cloudshell.configuration.cloudshell_snmp_binding_keys import SNMP_HANDLER
-from cloudshell.configuration.cloudshell_shell_core_binding_keys import LOGGER
-from cloudshell.shell.core.driver_context import AutoLoadDetails
-import os
 import re
 
+from cloudshell.devices.autoload.autoload_builder import AutoloadDetailsBuilder
+from cloudshell.devices.standards.networking.autoload_structure import GenericResource, GenericChassis, GenericPort, \
+    GenericPortChannel
 
-class AireOSAutoload(AutoloadOperationsInterface):
+
+class AireOSAutoload(object):
     PORT_DESCRIPTION_FILTER = [r'[Vv]irtual', r'[Cc]hannel']
     SUPPORTED_OS = [r'Controller']
 
-    def __init__(self, snmp_hander=None, logger=None):
-        self._snmp_handler = None
-        self.snmp_handler = snmp_hander
-        self._root = RootElement()
+    def __init__(self, snmp_handler, shell_name, shell_type, resource_name, logger):
+        self.shell_name = shell_name
+        self.shell_type = shell_type
+        self._content_indexes = None
+        self._if_indexes = None
+        self.logger = logger
+        self.snmp_handler = snmp_handler
+        self._resource_name = resource_name
+
+        self.resource = GenericResource(shell_name=shell_name,
+                                        shell_type=shell_type,
+                                        name=resource_name,
+                                        unique_id=resource_name)
+
         self._chassis = None
         self._ports = {}
         self._snmp_cache = {}
-        self._logger = logger
-
-        overridden_config = override_attributes_from_config(AireOSAutoload)
-        self.supported_os = overridden_config.SUPPORTED_OS
-
-    @property
-    def snmp_handler(self):
-        if self._snmp_handler is None:
-            self.snmp_handler = inject.instance(SNMP_HANDLER)
-        return self._snmp_handler
-
-    @snmp_handler.setter
-    def snmp_handler(self, snmp_handler):
-        if snmp_handler:
-            self._snmp_handler = snmp_handler
-            path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'mibs'))
-            self._snmp_handler.update_mib_sources(path)
-
-    @property
-    def logger(self):
-        if self._logger is not None:
-            return self._logger
-        return inject.instance(LOGGER)
 
     def _snmp_request(self, request_data):
         if isinstance(request_data, tuple):
@@ -75,14 +56,12 @@ class AireOSAutoload(AutoloadOperationsInterface):
         return False
 
     def _build_root_elements(self):
-        root_attributes = dict()
-        root_attributes[RootAttributes.CONTACT_NAME] = self._snmp_request(('SNMPv2-MIB', 'sysContact', '0'))
-        root_attributes[RootAttributes.SYSTEM_NAME] = self._snmp_request(('SNMPv2-MIB', 'sysName', '0'))
-        root_attributes[RootAttributes.LOCATION] = self._snmp_request(('SNMPv2-MIB', 'sysLocation', '0'))
-        root_attributes[RootAttributes.OS_VERSION] = self._snmp_request(('ENTITY-MIB', 'entPhysicalSoftwareRev', '1'))
-        root_attributes[RootAttributes.VENDOR] = self._snmp_request(('SNMPv2-MIB', 'sysObjectID', '0'))
-        root_attributes[RootAttributes.MODEL] = self._snmp_request(('ENTITY-MIB', 'entPhysicalDescr', '1'))
-        self._root.build_attributes(root_attributes)
+        self.resource.contact_name = self._snmp_request(('SNMPv2-MIB', 'sysContact', '0'))
+        self.resource.system_name = self._snmp_request(('SNMPv2-MIB', 'sysName', '0'))
+        self.resource.location = self._snmp_request(('SNMPv2-MIB', 'sysLocation', '0'))
+        self.resource.os_version = self._snmp_request(('ENTITY-MIB', 'entPhysicalSoftwareRev', '1'))
+        self.resource.vendor = self._snmp_request(('SNMPv2-MIB', 'sysObjectID', '0'))
+        self.resource.model = self._snmp_request(('ENTITY-MIB', 'entPhysicalDescr', '1'))
 
     def _build_chassis_elements(self):
 
@@ -99,18 +78,18 @@ class AireOSAutoload(AutoloadOperationsInterface):
                     entity_data = table
                     break
         else:
-            raise Exception('_build_chassis_elements', 'Entity table is empty')
+            raise Exception(self.__class__.__name__, 'Entity table is empty')
 
         if chassis_id and entity_data:
-            self._chassis = Chassis(chassis_id)
+            self._chassis = GenericChassis(shell_name=self.shell_name,
+                                           name="Chassis {}".format(chassis_id),
+                                           unique_id="{0}.{1}.{2}".format(self._resource_name, "chassis", chassis_id))
         else:
-            raise Exception('_build_chassis_elements', 'Cannot find chassis data in entity table')
+            raise Exception(self.__class__.__name__, 'Cannot find chassis data in entity table')
 
-        chassis_attributes = dict()
-        chassis_attributes[ChassisAttributes.MODEL] = self._get_from_table('entPhysicalModelName', entity_data)
-        chassis_attributes[ChassisAttributes.SERIAL_NUMBER] = self._get_from_table('entPhysicalSerialNum', entity_data)
-        self._chassis.build_attributes(chassis_attributes)
-        self._root.chassis.append(self._chassis)
+        self._chassis.model = self._get_from_table('entPhysicalModelName', entity_data)
+        self._chassis.serial_number = self._get_from_table('entPhysicalSerialNum', entity_data)
+        self.resource.add_sub_resource(chassis_id, self._chassis)
 
     def _build_ports(self):
         if_mib_table = self._snmp_request(('IF-MIB', 'ifTable'))
@@ -120,22 +99,23 @@ class AireOSAutoload(AutoloadOperationsInterface):
             if self._match_pattern_list(self.PORT_DESCRIPTION_FILTER, port_description):
                 break
             port_index = self._get_from_table('ifIndex', table)
-            port = Port(port_index, self._convert_port_description(port_description))
-            port_attributes = dict()
-            port_attributes[PortAttributes.PORT_DESCRIPTION] = self._snmp_request(('IF-MIB', 'ifAlias', index))
-            port_attributes[PortAttributes.L2_PROTOCOL_TYPE] = str(self._get_from_table('ifType', table)).replace(
-                """'""", '')
-            port_attributes[PortAttributes.MAC_ADDRESS] = self._get_from_table('ifPhysAddress', table)
-            port_attributes[PortAttributes.MTU] = self._get_from_table('ifMtu', table)
-            port_attributes[PortAttributes.BANDWIDTH] = self._get_from_table('ifSpeed', table)
-            port_attributes[PortAttributes.IPV4_ADDRESS] = self._find_associated_ipv4(index)
-            port_attributes[PortAttributes.IPV6_ADDRESS] = self._find_associated_ipv6(index)
-            port_attributes[PortAttributes.DUPLEX] = self._get_duplex(index)
-            port_attributes[PortAttributes.AUTO_NEGOTIATION] = self._get_autonegotiation(index)
-            # port_attributes[PortAttributes.ADJACENT] = self._get_adjacent(index)
-            port.build_attributes(port_attributes)
+            port = GenericPort(shell_name=self.shell_name,
+                               name=self._convert_port_description(port_description),
+                               unique_id='{0}.{1}.{2}'.format(self._resource_name, 'port', port_index))
+
+            # port = Port(port_index, self._convert_port_description(port_description))
+            # port_attributes = dict()
+            port.port_description = self._snmp_request(('IF-MIB', 'ifAlias', index))
+            port.l2_protocol_type = str(self._get_from_table('ifType', table)).replace("""'""", '')
+            port.mac_address = self._get_from_table('ifPhysAddress', table)
+            port.mtu = self._get_from_table('ifMtu', table)
+            port.bandwidth = self._get_from_table('ifSpeed', table)
+            port.ipv4_address = self._find_associated_ipv4(index)
+            port.ipv6_address = self._find_associated_ipv6(index)
+            port.duplex = self._get_duplex(index)
+            port.auto_negotiation = self._get_autonegotiation(index)
             self._ports[port_index] = port
-            self._chassis.ports.append(port)
+            self._chassis.add_sub_resource(port_index, port)
 
     def _build_port_channels(self):
         if_mib_table = self._snmp_request(('IF-MIB', 'ifTable'))
@@ -143,15 +123,21 @@ class AireOSAutoload(AutoloadOperationsInterface):
             description = table['ifDescr']
             if re.search(r'[Cc]hannel', description) and '.' not in description:
                 suitable_description = self._convert_port_description(description)
-                port = PortChannel(self._get_from_table('ifIndex', table), suitable_description)
-                pc_attributes = dict()
-                pc_attributes[PortChannelAttributes.PORT_DESCRIPTION] = self._snmp_request(('IF-MIB', 'ifAlias', index))
-                pc_attributes[PortChannelAttributes.PROTOCOL_TYPE] = self._get_from_table('ifType', table)
-                pc_attributes[PortChannelAttributes.IPV4_ADDRESS] = self._find_associated_ipv4(index)
-                pc_attributes[PortChannelAttributes.IPV6_ADDRESS] = self._find_associated_ipv6(index)
-                pc_attributes[PortChannelAttributes.ASSOCIATED_PORTS] = self._get_associated_ports(index)
-                port.build_attributes(pc_attributes)
-                self._root.port_channels.append(port)
+                port_channel_index = self._get_from_table('ifIndex', table)
+                port_channel = GenericPortChannel(shell_name=self.shell_name,
+                                                  name=suitable_description,
+                                                  unique_id='{0}.{1}.{2}'.format(self._resource_name,
+                                                                                 'port_channel',
+                                                                                 port_channel_index))
+
+                # port = PortChannel(self._get_from_table('ifIndex', table), suitable_description)
+                # pc_attributes = dict()
+                port_channel.port_description = self._snmp_request(('IF-MIB', 'ifAlias', index))
+                # pc_attributes[PortChannelAttributes.PROTOCOL_TYPE] = self._get_from_table('ifType', table)
+                port_channel.ipv4_address = self._find_associated_ipv4(index)
+                port_channel.ipv6_address = self._find_associated_ipv6(index)
+                port_channel.associated_ports = self._get_associated_ports(index)
+                self.resource.add_sub_resource(port_channel_index, port_channel)
 
     def _get_duplex(self, index):
         duplex_table = self._snmp_request(('EtherLike-MIB', 'dot3StatsDuplexStatus'))
@@ -236,14 +222,14 @@ class AireOSAutoload(AutoloadOperationsInterface):
     def _convert_port_description(self, description):
         return description.replace('/', '-').replace(' ', '').replace(':', '-')
 
-    def _is_valid_device_os(self):
+    def _is_valid_device_os(self, supported_os):
         """Validate device OS using snmp
             :return: True or False
         """
 
         system_description = self._snmp_request(('SNMPv2-MIB', 'sysDescr', 0))
         self.logger.debug('Detected system description: \'{0}\''.format(system_description))
-        result = re.search(r"({0})".format("|".join(self.supported_os)),
+        result = re.search(r"({0})".format("|".join(supported_os)),
                            system_description,
                            flags=re.DOTALL | re.IGNORECASE)
 
@@ -251,17 +237,18 @@ class AireOSAutoload(AutoloadOperationsInterface):
             return True
         else:
             error_message = 'Incompatible driver! Please use this driver for \'{0}\' operation system(s)'. \
-                format(str(tuple(self.supported_os)))
+                format(str(tuple(supported_os)))
             self.logger.error(error_message)
             return False
 
-    def discover(self):
-        if not self._is_valid_device_os():
+    def discover(self, supported_os):
+        if not self._is_valid_device_os(supported_os):
             raise Exception(self.__class__.__name__, 'Unsupported device OS, see logs for more details')
         self._build_root_elements()
         self._build_chassis_elements()
         self._build_ports()
         self._build_port_channels()
-        self._root.build_relative_path()
-        autoload_details = AutoLoadDetails(self._root.get_resources(), self._root.get_attributes())
+        # self._root.build_relative_path()
+        autoload_details = AutoloadDetailsBuilder(self.resource).autoload_details()
+        # autoload_details = AutoLoadDetails(self._root.get_resources(), self._root.get_attributes())
         return autoload_details
